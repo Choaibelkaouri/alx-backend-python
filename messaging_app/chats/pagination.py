7871834ae91e03d2aef1b49d03989182f0f48ae8
CHOAIB EL KAOURI <65832846+Choaibelkaouri@users.noreply.github.com>
2025-11-17 20:46:11 +0100

    Delete messaging_app directory

--- messaging_app/chats/pagination.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.pagination import PageNumberPagination
-
-class MessagePagination(PageNumberPagination):
-    # Paginates message lists: 20 messages per page.
-    page_size = 20
-    page_query_param = 'page'
-    max_page_size = 100
