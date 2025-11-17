a169bbfac311097f5d6b625b31b2c05e2573ed11
CHOAIB EL KAOURI <65832846+Choaibelkaouri@users.noreply.github.com>
2025-11-17 21:38:10 +0100

    Update settings.py

--- Django-Middleware-0x03/messaging_app/settings.py
+++ Django-Middleware-0x03/messaging_app/settings.py
@@ -1,3 +1,20 @@
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    # Custom middleware
+    'chats.middleware.RequestLoggingMiddleware',
+    # (ممكن بعدها تضيف الباقي later)
+    # 'chats.middleware.RestrictAccessByTimeMiddleware',
+    # 'chats.middleware.OffensiveLanguageMiddleware',
+    # 'chats.middleware.RolepermissionMiddleware',
+]
+
 from pathlib import Path
 from datetime import timedelta
 import os
